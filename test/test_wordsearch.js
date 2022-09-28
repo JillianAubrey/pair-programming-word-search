9cdc54a2addeedb8b123f3591c3646c232044905
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 22:42:52 +0000

    Implemented finding of diagonal words

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
@@ -41,7 +41,7 @@ describe("#wordSearch()", function() {
       ['a', 'b', 'c'],
       ['n', 'q', 'r'],
       ['d', 'r', 'p']
-    ], 'and')
+    ], 'and');
 
     assert.isTrue(result);
   });
@@ -57,7 +57,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'YEN')
+    ], 'YEN');
 
     assert.isTrue(result);
   });
@@ -73,13 +73,80 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'WEST')
+    ], 'WEST');
 
     assert.isTrue(result);
   });
 
+  it("should return true if the word is present on a diagonal in any direction", function() {
+    let result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'QUAB');
+    
+    assert.isTrue(result);
 
-  // [[a, b, c]
-// [n, q, r]
-// [d, r, p]]
+    result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'YEAS');
+
+    assert.isTrue(result);
+
+    result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'RUFF');
+
+    assert.isTrue(result);
+
+    result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FIFH');
+
+    assert.isTrue(result);
+
+  });
 });
+
+// [
+//   'AECTYEAS', 'EDTEYVAD', 'WIFEEYI',
+//   'ZCWNERL',  'CNQVRB',   'KAAERG',
+//   'FFURL',    'FKPYL',    'QEAG',
+//   'QUAB',     'ULL',      'UAI',
+//   'AD',       'AS',       'AECTYEAS',
+//   'EDTEYVAD', 'SFJSNPAL', 'OBRSEULL',
+//   'YMCEAUA',  'UFCTQEA',  'HHRWKU',
+//   'BHJFFU',   'WFTAQ',    'WMCNQ',
+//   'BBCF',     'HFIF',     'UDK',
+//   'YEC',      'OZ',       'SW'
+// ]
--- wordsearch.js
+++ wordsearch.js
@@ -1,23 +1,50 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-        if (l.includes(reverseString(word))) return true
-    }
-    const lettersWidth = letters[0].length;
-    for(i = 0; i < lettersWidth; i++) {
-        const column = letters.map((item)=>{
-            return item[i]
-        }).join('');
-        if (column.includes(word)) return true;
-        if (column.includes(reverseString(word))) return true;
-    }
+const wordSearch = (letters, word) => {
+  const lettersWidth = letters[0].length;
+  const wordRev = reverseString(word);
+  //checking for horizontal word
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+    if (l.includes(wordRev)) return true;
+  }
+  //checking for vertical word
+  for (let i = 0; i < lettersWidth; i++) {
+    const column = letters.map((item)=>{
+      return item[i];
+    }).join('');
+    if (column.includes(word)) return true;
+    if (column.includes(wordRev)) return true;
+  }
+  //checking for diagonal words
+  const diagonals = getDiagonals(letters);
+  for (const diagonal of diagonals) {
+    if (diagonal.includes(word)) return true;
+    if (diagonal.includes(wordRev)) return true;
+  }
 
-    return false;
-}
+  return false;
+};
 
 const reverseString = function(str) {
-    return str.split('').reverse().join('');
-}
+  return str.split('').reverse().join('');
+};
 
-module.exports = wordSearch
+const getDiagonals = function(arr) {
+  let diagonals = [];
+  const arrWidth = arr[0].length;
+  const arrHeight = arr.length;
+  const arrRev = [...arr].reverse();
+
+  for (let i = 0; i < arrHeight; i++) {
+    diagonals.push(arr.map((item, index) => item[index + i]).join(''));
+    diagonals.push(arrRev.map((item, index) => item[index + i]).join(''));
+  }
+  for (let j = 0; j < arrWidth; j++) {
+    diagonals.push(arr.map((item, index) => item[index - j]).join(''));
+    diagonals.push(arrRev.map((item, index) => item[index - j]).join(''));
+  }
+  return diagonals.filter((digonal) => digonal.length > 1);
+};
+  
+  
+module.exports = wordSearch;
